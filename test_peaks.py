4f4ef945f446817bd86c958d0188c4ee1c6c08b5
Tristan Britt <tristan.britt@mail.mcgill.ca>
2020-12-22 12:23:17 -0500

    refactored to object based analysis

--- test_peaks.py
+++ test_peaks.py
@@ -19,8 +19,46 @@
 # =====================================================================
 
 from image import *
-import matplotlib.image as mpimg
 from skimage import img_as_float
+
+def plot_img_and_hist(image, axes, bins=256):
+    """Plot an image along with its histogram and cumulative histogram.
+    Parameters
+    ----------
+    image : ndarray ([M[, N[, ...P]][, C]) of ints, uints or floats
+    axes  : matplotlib.pyplo.axes objects to be used to plot results 
+    bins  : int of bins used in numpy histogram call for plotting on 
+            matplotlib axis object
+
+    Returns
+    -------
+    ax_img  : axes object to plot image
+    ax_hist : axes object to plot histogram
+    ax_cdf  : axes object to plot cumulative distribution function
+
+    """
+    image = img_as_float(image)
+    ax_img, ax_hist = axes
+    ax_cdf = ax_hist.twinx()
+
+    # Display image
+    ax_img.imshow(image, cmap=plt.cm.gray)
+    ax_img.set_axis_off()
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore")
+        # Display histogram
+        ax_hist.hist(image.ravel(), bins=bins, histtype="step", color="black")
+        ax_hist.ticklabel_format(axis="y", style="scientific", scilimits=(0, 0))
+        ax_hist.set_xlabel("Pixel intensity")
+        ax_hist.set_yticks([])
+
+        # Display cumulative distribution
+        img_cdf, bins = exposure.cumulative_distribution(image, bins)
+        ax_cdf.plot(bins, img_cdf, "r")
+        ax_cdf.set_yticks([])
+
+    return ax_img, ax_hist, ax_cdf
+
 
 if __name__ == "__main__":
     # ============================================
@@ -31,19 +69,12 @@ if __name__ == "__main__":
     test_color = True
     if test_color:
         RGB_image = "test_RGB.jpg"
+        test_color = labeled_image(RGB_image)
+        i,j = test_color.peaks(alpha=20, size=10)
+        lattice_points, idx = test_color.labeled_peaks(i,j)
 
-        # reading the image
-        orig = img_as_float(mpimg.imread(RGB_image))
-        gamma_corrected = rgb2gamma(orig)
-        width, height = gamma_corrected.shape
-        # computing the standard deviation of the image
-        sigma = get_std(gamma_corrected)
-        # getting the peaks
-        i, j = get_max(gamma_corrected, sigma, alpha=20, size=10)
-        lattice_points, idx = labeled_peaks(i,j, width, height)
         # let's see the results
         # Display results
-
         fig = plt.figure(figsize=(12, 8))
         axes = np.zeros((2, 2), dtype=np.object)
         axes[0, 0] = plt.subplot(2, 2, 1)
@@ -51,14 +82,14 @@ if __name__ == "__main__":
         axes[1, 0] = plt.subplot(2, 2, 3)
         axes[1, 1] = plt.subplot(2, 2, 4)
 
-        ax_img, ax_hist, ax_cdf = plot_img_and_hist(orig, axes[:, 0])
+        ax_img, ax_hist, ax_cdf = plot_img_and_hist(test_color.data, axes[:, 0])
         ax_img.set_title("Low contrast image")
 
         y_min, y_max = ax_hist.get_ylim()
         ax_hist.set_ylabel("Number of pixels")
         ax_hist.set_yticks(np.linspace(0, y_max, 5))
 
-        ax_img, ax_hist, ax_cdf = plot_img_and_hist(gamma_corrected, axes[:, 1])
+        ax_img, ax_hist, ax_cdf = plot_img_and_hist(test_color.corrected_data, axes[:, 1])
         
 
         ax_img.plot(lattice_points[:, 0], lattice_points[:, 1], "gx", ms=10)
@@ -85,17 +116,12 @@ if __name__ == "__main__":
     if test_bw:
         BW_image = "test_BW.jpg"
 
-        test = img_as_float(mpimg.imread(BW_image))
-        # computing the standard deviation of the image
-        width, height = grey2gamma(test).shape
-
-        sigma = get_std(grey2gamma(test))
-        # getting the peaks
-        i, j = get_max(grey2gamma(test), sigma, alpha=10, size=10)
-        lattice_points, idx = labeled_peaks(i,j, width, height)
+        test_BW = labeled_image(BW_image)
+        i, j = test_BW.peaks(alpha=10, size=10)
+        lattice_points, idx = test_BW.labeled_peaks(i,j)
 
         fig, ax = plt.subplots(1, 2, figsize=(8, 6))
-        ax[0].imshow(test, cmap=plt.cm.gray)
+        ax[0].imshow(test_BW.data, cmap=plt.cm.gray)
         ax[0].set_axis_off()
         ax[0].set_title("Original")
         
@@ -108,7 +134,7 @@ if __name__ == "__main__":
             for m in range(len(idx))
         ]
 
-        ax[1].imshow(grey2gamma(test), cmap=plt.cm.gray)
+        ax[1].imshow(test_BW.corrected_data, cmap=plt.cm.gray)
         ax[1].set_axis_off()
         ax[1].plot(i, j, "ro", markersize=10, alpha=0.5)
         ax[1].set_title("Greyscale + Gamma Corrected")
